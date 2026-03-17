40a2b9edcb64e7f02abed063db691d813cef7a92
hassen344 <karouidesign867@gmail.com>
2026-03-17 03:10:42 +0100

    fix: production build config

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,6 +5,14 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+    port: 10000,
+    allowedHosts: [
+      "karoui-hassen2.onrender.com"
+    ]
+  },
+  preview: {
+    host: true,
+    port: 10000,
     allowedHosts: [
       "karoui-hassen2.onrender.com"
     ]
