e11f9be9b0b964b9a5d3ebcb851da8ddafdc0c4e
hassen344 <karouidesign867@gmail.com>
2026-03-17 02:32:16 +0100

    fix: update backend URL

--- back/server.js
+++ back/server.js
@@ -9,12 +9,14 @@ const app = express();
 app.use(cors({ 
   origin: [
     'http://localhost:5173',
-    'https://karoui-hassen2.onrender.com  '
+    'https://karoui-hassen2.onrender.com',
+    
+    'https://portfolio-2-mo22.onrender.com'
   ]
-})); // port Vite
+}));
 app.use(express.json());
 app.use('/api', emailRoute);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Serveur lancé sur le port ${process.env.PORT || 5000}`);
+  console.log(`Serveur lancé sur le port ${process.env.PORT || 3000}`);
 });
